d49cd526ad183007f91acd0fd17ddf10b009ae33
Anthony Alvarez <anthonyalvarez@my.whccd.edu>
2024-01-24 19:04:35 -0800

    test

--- COMP003A/Program.cs
+++ COMP003A/Program.cs
@@ -12,9 +12,28 @@ namespace COMP003A
         static void Main(string[] args)
         {
             /* working with variables */
-            // the following code shows all three primary variable-related activities:
+            // the following code shows all three primary variable-related activities
+            string username; // declaring a variable (1 of 3)
+            Console.WriteLine("What is your name?"); // request for user input
+            username = Console.ReadLine(); // retrieving its current value (2 of 3)
+            Console.WriteLine("Hi " + username); // retrieving its current value (3 of 3)
 
-            Console.WriteLine("Hello, World!");
+
+            // another example of the three primary variable related activities (declaring, assigning, and retrieving)
+            string favoriteColor;
+            Console.WriteLine("What is your favorite color?");
+            favoriteColor = Console.ReadLine();
+            Console.WriteLine("Your favorite color is " + favoriteColor);
+
+            /* working with variable integers */
+            int score; // declare a new variable with a data type int
+            // score - "Generic User"; // will fail to compile because the "O" is still assigned as a string - enclosed in the double quotes
+            score = 0; // works
+            score = 4; // you can also reassign values to the variable as long as they are within scope and not constant
+            score = 11;
+            score = -1564;
+
+            // test
         }
     }
 }
