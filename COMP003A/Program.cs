3f9703b34764db1454f4224ccc2d2d4106432522
anthonyalvarez <anthonyalvarez@LEM278-21>
2024-01-24 18:41:37 -0800

    Add project files.

---
+++ COMP003A/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Author: Anthony Alvarez
+ * Course: COMP-003A
+ * Purpose: Lecture activity for variables, type system, Math, and Console properties
+ * Reference: The C# Player's Guide (4e) by RB Whitaker
+ */
+
+namespace COMP003A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* working with variables */
+            // the following code shows all three primary variable-related activities:
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
